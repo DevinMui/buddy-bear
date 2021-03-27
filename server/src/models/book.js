d200e787563c67b72769ab63f5d8e1df9d11f49a
Devin Mui <devinwmui@gmail.com>
2021-03-27 15:21:58 -0700

    FUCK YOU

--- server/src/models/book.js
+++ server/src/models/book.js
@@ -16,6 +16,7 @@ const schema = mongoose.Schema(
                 enum: ['SYNTHESIZED', 'RECORDED'],
                 default: 'SYNTHESIZED',
             },
+            files: [{}],
         },
         mode: {
             type: String,
--- server/src/routes/api/books.js
+++ server/src/routes/api/books.js
@@ -1,16 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import multer from 'multer'
 import Book from '../../models/book'
 import Page from '../../models/page'
 
 let router = express.Router()
+
+const upload = multer({ dest: 'uploads/' })
 
 router.get('/', async (req, res) => {
     const books = await Book.find({ user: mongoose.ObjectId(req.user._id) })
     res.json({ status: 'success', data: books })
 })
 
-router.post('/', async (req, res) => {
+// multipart/form-data
+router.post('/', upload.array('audio'), async (req, res) => {
+    let body = req.body
+    body.audio.files = req.files
     const book = await new Book(req.body).save()
     res.json({ status: 'success', data: book })
 })
@@ -25,8 +31,12 @@ router.get('/:id/pages', async (req, res) => {
     res.json({ status: 'success', data: pages })
 })
 
-router.post('/:id/pages', async (req, res) => {
-    const page = await new Page(req.body).save()
+// multipart/form-data
+router.post('/:id/pages', upload.single('audio'), async (req, res) => {
+    const file = req.file.path
+    let body = req.body
+    body.text.file = file
+    const page = await new Page(body).save()
     res.json({ status: 'success', data: page })
 })
 
