c87ac6afcf18b2df0703f607a21d2568934ce877
Devin Mui <devinwmui@gmail.com>
2021-03-27 16:05:00 -0700

    center

--- server/src/routes/api/books.js
+++ server/src/routes/api/books.js
@@ -8,40 +8,58 @@ let router = express.Router()
 
 const upload = multer({ dest: 'uploads/' })
 
-router.get('/search', async (req, res) => {
-    
-})
+router.get('/search', async (req, res) => {})
 
-router.get('/', async (req, res) => {
-    const books = await Book.find({ user: mongoose.ObjectId(req.user._id) })
-    res.json({ status: 'success', data: books })
-})
-
-// multipart/form-data
-router.post('/', upload.array('audio'), async (req, res) => {
-    let body = req.body
-    body.audio.files = req.files
-    const book = await new Book(req.body).save()
-    res.json({ status: 'success', data: book })
-})
-
-router.get('/:id', async (req, res) => {
-    const book = await Book.findById(req.params.id)
-    res.json({ status: 'success', data: book })
-})
-
-router.get('/:id/pages', async (req, res) => {
-    const pages = await Page.find({ book: mongoose.ObjectId(req.user._id) })
-    res.json({ status: 'success', data: pages })
+router.get('/', async (req, res, next) => {
+    try {
+        const books = await Book.find({ user: mongoose.ObjectId(req.user._id) })
+        res.json({ status: 'success', data: books })
+    } catch (e) {
+        next(e)
+    }
 })
 
 // multipart/form-data
-router.post('/:id/pages', upload.single('audio'), async (req, res) => {
-    const file = req.file.path
-    let body = req.body
-    body.text.file = file
-    const page = await new Page(body).save()
-    res.json({ status: 'success', data: page })
+router.post('/', upload.array('audio'), async (req, res, next) => {
+    try {
+        let body = req.body
+        body.audio.files = req.files
+        const book = await new Book(req.body).save()
+        res.json({ status: 'success', data: book })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        res.json({ status: 'success', data: book })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.get('/:id/pages', async (req, res, next) => {
+    try {
+        const pages = await Page.find({ book: mongoose.ObjectId(req.user._id) })
+        res.json({ status: 'success', data: pages })
+    } catch (e) {
+        next(e)
+    }
+})
+
+// multipart/form-data
+router.post('/:id/pages', upload.single('audio'), async (req, res, next) => {
+    try {
+        const file = req.file.path
+        let body = req.body
+        body.text.file = file
+        const page = await new Page(body).save()
+        res.json({ status: 'success', data: page })
+    } catch (e) {
+        next(e)
+    }
 })
 
 export default router
