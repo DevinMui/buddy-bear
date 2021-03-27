b517b843422726d9635f8e97fc8fbc5af952a731
aaron <huang.aaron03@gmail.com>
2021-03-27 01:26:05 -0700

    kids ui

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -39,6 +39,9 @@ function App() {
                         <Route exact path="/">
                             <Home />
                         </Route>
+                        <Route exact path="/kids">
+                            <Kids/>
+                        </Route>
                     </>
                 ) : (
                     <>
--- web/src/pages/kids/index.js
+++ web/src/pages/kids/index.js
@@ -1,5 +1,6 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import Paw from './paw.svg'
+import Webcam from "react-webcam";
 
 const Background = styled.div`
   background: var(--primary-color);
@@ -7,23 +8,74 @@ const Background = styled.div`
   width: 100%;
 `;
 
+const Paw = styled.img`
+  position: fixed;
+  height: 80vh;
+  right: 0;
+  bottom: -10px;
+`;
+
+const Preview = styled.div`
+  height: 33vw;
+  width: 33vw;
+  padding: 0;
+  position: fixed;
+  top: 24px;
+`;
+
+const CameraWarning = styled.div`
+  margin: 1rem;
+  opacity: 0.8;
+  text-align: center;
+`;
+
 export default function () {
   // Put some warnings here for rotation
   // text
   // camera
   // paw
+  const camRef = useRef(null);
+  const [cameraActive, setCameraActive] = useState(false);
+  const [isLandscape, setIsLandscape] = useState(true)
+
+  useEffect(() => {
+       const e = function() {
+        console.log(window.orientation)
+        setIsLandscape(window.orientation % 180 !== 0)
+    }
+    e()
+    window.addEventListener("resize", e);
+    return window.removeEventListener('resize', e)
+    
+   }, [])
+
   return (
     <Background>
-      <div className="container">
+      <div
+        className="visible-xs-block container row"
+        style={{ margin: "0 auto" }}
+      >
+        <Paw src={require("./paw.svg").default} />
         <div
-          className="col-4"
+          className={isLandscape? "col-4":'col-12'}
           style={{ paddingTop: "2rem", paddingBottom: "2rem" }}
         >
           <h1>Hello!</h1>
-          <h2>Some instruction text for the child.</h2>
+          <h2>{isLandscape? 'Put me in your bear\'s paw.' : 'Rotate the phone to start reading.'}</h2>
         </div>
-        <div className="col-8">
-            <img src={Paw}/>
+        <div className="col-6 offset-5" style={{display: isLandscape ? 'block':'none'}}>
+          <Preview className="card-i bg-color d-flex justify-content-center align-items-center">
+            <Webcam
+              ref={camRef}
+              onUserMediaError={() => setCameraActive(false)}
+              onUserMedia={() => setCameraActive(true)}
+            />
+            {cameraActive || (
+              <CameraWarning>
+                Please enable your camera to continue.
+              </CameraWarning>
+            )}
+          </Preview>
         </div>
       </div>
     </Background>
