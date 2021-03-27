3241c0d1adf0d10bdf74bd9d39fe4da2f68dbbaa
Devin Mui <devinwmui@gmail.com>
2021-03-27 01:57:06 -0700

    how are there vulnerabilities

--- web/src/pages/book/comparison.js
+++ web/src/pages/book/comparison.js
@@ -1,3 +1,5 @@
+import Diff from 'react-stylable-diff'
+
 export default function Comparison() {
     return (
         <div className="mt-4">
@@ -5,7 +7,13 @@ export default function Comparison() {
                 <div className="col-md-6">
                     <h3 className="text-center">Your child</h3>
                     <div className="comparison-card mt-4">
-                        <p>Lorem Ipsum</p>
+                        <p>
+                            <Diff
+                                inputA="Lorem Ipsum"
+                                inputB="Lorem Ip failure"
+                                type="words"
+                            />
+                        </p>
                         <button className="btn play">
                             <i className="bi bi-play-fill"></i>
                         </button>
@@ -22,6 +30,14 @@ export default function Comparison() {
                             <i className="bi bi-play-fill"></i>
                         </button>
                     </div>
+                </div>
+            </div>
+            <div className="mt-4">
+                <div className="float-left">
+                    <button className="btn btn-link">Previous Page</button>
+                </div>
+                <div className="float-right">
+                    <button className="btn btn-link">Next Page</button>
                 </div>
             </div>
         </div>
