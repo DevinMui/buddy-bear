957d2c7388ed2a064f994b06b1d614647ea2b23a
Jaiveer Singh <j.singh@berkeley.edu>
2021-03-28 02:57:05 -0700

    Updated lots of state machine code

--- device/main.py
+++ device/main.py
@@ -11,32 +11,95 @@ ser = serial.Serial(
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
-
 sio = socketio.Client()
 
 ARDUINO_TIMEOUT = 2
 
+ARDUINO_ACK_SUCCESS = "ACK_SUCCESS"
+ARDUINO_ACK_FAILURE = "ACK_FAILURE"
+
+ARDUINO_COMMAND_SCAN = "CMD_SCAN"
+ARDUION_COMMAND_WAVE = "CMD_WAVE"
+ARDUINO_COMMAND_DANCE = "CMD_DANCE" 
+ARDUINO_COMMAND_BUZZ = "CMD_BUZZ"
+ARDUINO_COMMAND_BUZZ_LONG = "CMD_BUZZ_LONG"
+
+ARDUINO_NOTIFICATION_BUTTON = "NTF_BUTTON"
+
+STATE_OFF = "STATE_OFF"
+STATE_READING = "STATE_READING"
+
+EVENT_IDLE = "EVENT_IDLE"
+EVENT_BUTTON = "EVENT_BUTTON"
+EVENT_FINISHED = "EVENT_FINISHED"
+
+current_state = STATE_OFF
+
 def send_arduino_command(command):
     ser.write(command)
-    ser.readline(ARDUINO_TIMEOUT)
+    ack = ser.readline(ARDUINO_TIMEOUT)
+    if ack != ARDUINO_ACK_SUCCESS:
+        print(f"Arduino failed to acknowledge! Command: {command}")
+        return False
+    return True
 
-def scan_page():
-    send_arduino_command("SCAN")
+def handle_arduino_notification(notification):
+    if notification == ARDUINO_NOTIFICATION_BUTTON:
+        handle_event(EVENT_BUTTON)
+    else:
+        print(f"Arduino sent unknown notification! Notification: {notification}")
+        return False
+    
+    return True
 
-def wave():
-    send_arduino_command("WAVE")
+def handle_event(event):
 
-def dance():
-    send_arduino_command("DANCE")
+    def log_unexpected_event():
+        print(f"Unexpected event! Received {event} while in state {current_state}")
+
+    next_state = current_state
+    if event == EVENT_BUTTON:
+        if current_state == STATE_OFF: 
+            # If currently off, turn bear on and wave
+            send_arduino_command(ARDUINO_COMMAND_BUZZ_LONG)
+            send_arduino_command(ARDUINO_COMMAND_WAVE)
+
+            # Transition to reading state next
+            next_state = STATE_READING
+
+        elif current_state == STATE_READING:
+            # If currently reading, scan new page
+            send_arduino_command(ARDUINO_COMMAND_BUZZ)
+            send_arduino_command(ARDUINO_COMMAND_SCAN)
+        
+        else:
+            log_unexpected_event()
+    
+    elif event == EVENT_FINISHED:
+        if current_state == STATE_READING:
+            # If currently reading, perform a celebration
+            send_arduino_command(ARDUINO_COMMAND_DANCE)
+
+            # Transition to off state next
+            next_state = STATE_OFF
+        else:
+            log_unexpected_event()
+    
+    elif event == EVENT_IDLE:
+        # Currently do nothing on idle event
+        pass
+        
+    # Transition to next state
+    current_state = next_state
+
 
 @sio.event
 def connect():
     print('connection established')
 
 @sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+def reward(data):
+    print('Received reward with data:', data)
 
 @sio.event
 def disconnect():
@@ -46,11 +109,16 @@ def main():
     text = ser.readline()
     print(f"Arduino sent: {text}")
 
-    ser.close()
-
     sio.connect('http://192.168.0.20:5000')
-    sio.emit("join", "hi")
-    sio.wait()
+
+    while True:
+        if ser.in_waiting():
+            notification = ser.readline(ARDUINO_TIMEOUT)
+            handle_arduino_notification(notification)
+        else:
+            handle_event(EVENT_IDLE)
+
+        time.sleep(0.25)
 
 if __name__ == "__main__":
     main()
